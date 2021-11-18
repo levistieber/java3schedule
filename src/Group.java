9dfa02992e615bf03f7580cbcc06e68fb4eb8fcc
levistieber <72553310+levistieber@users.noreply.github.com>
2021-11-18 09:44:03 +0100

    Update Group.java

--- src/Group.java
+++ src/Group.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Group {
+    private String name;
+    private ArrayList<Student> members;
+
+    public Group(String name, ArrayList<Student> members) {
+        this.name = name;
+        this.members = members;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Student> getMembers() {
+        return members;
+    }
+
+    public void addStudent(Student student) {
+        this.members.add(student);
+    }
+    
+    public void removeStudent(Student student) {
+        this.members.remove(student);
+    }
 }
