3d52496bcaff5f97947e70245c86d05b0978a801
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2021-11-18 09:24:02 +0100

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,15 @@
 public class Room {
+    private String name;
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
