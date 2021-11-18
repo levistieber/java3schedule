e216b585e06bd420707634ebb12498e0926e6849
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2021-11-18 09:35:09 +0100

    people update

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,30 @@
 public class Student extends Person{
+    private String name;
+    private int number;
+
+    public Student(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void doSomething() {
+
+    }
 }
