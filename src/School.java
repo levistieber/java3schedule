da86b702f34007bd619138667024dcb444ac2b18
levistieber <72553310+levistieber@users.noreply.github.com>
2021-11-18 09:28:18 +0100

    Update School.java

--- src/School.java
+++ src/School.java
@@ -1,2 +1,15 @@
 public class School {
+    private String name;
+
+    public School(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
